50a1beed2f6653c8e9e977f439b81ff41bdfc2b2
Larissa Gomes <larissagomesgaspar@gmail.com>
2026-02-13 18:06:08 -0300

    Feat: Alteração completa das views que estavam com erros (todas funcionais agora)

--- sql/03_Views_e_Procedures.sql
+++ sql/03_Views_e_Procedures.sql
@@ -1,9 +1,5 @@
 USE MusicMetrics;
 
--- ============================================
--- VIEWS ANALÍTICAS
--- ============================================
-
 -- View: Análise completa de músicas com features
 CREATE OR REPLACE VIEW vw_tracks_complete AS
 SELECT 
@@ -67,20 +63,22 @@ CREATE OR REPLACE VIEW vw_top_artists_with_tracks AS
 SELECT 
     a.artist_id,
     a.artist_name,
-    a.artist_genres AS genres,
+    a.genres,
     a.popularity,
     a.followers,
     COUNT(t.track_id) as total_tracks,
-    AVG(t.popularity) as avg_track_popularity,
+    ROUND(AVG(t.popularity), 1) as avg_track_popularity,
     MAX(t.popularity) as max_track_popularity
 FROM dim_artists a LEFT JOIN dim_tracks t ON a.artist_id = t.artist_id
-GROUP BY a.artist_id, a.artist_name, a.artist_genres, a.popularity, a.followers
+WHERE a.popularity > 0  -- ← Artista com alguma popularidade
+GROUP BY a.artist_id, a.artist_name, a.genres, a.popularity, a.followers
+HAVING total_tracks > 0  -- ← Filtro de artistas com músicas
 ORDER BY a.popularity DESC;
 
 -- View: Evolução musical por década
 CREATE OR REPLACE VIEW vw_music_by_decade AS
 SELECT 
-    FLOOR(t.release_year / 10) * 10 as decade,
+    FLOOR(YEAR(t.release_date) / 10) * 10 as decade,
     COUNT(t.track_id) as total_tracks,
     AVG(t.popularity) as avg_popularity,
     AVG(t.duration_ms / 60000) as avg_duration_min,
@@ -90,23 +88,30 @@ SELECT
     AVG(af.tempo) as avg_tempo,
     AVG(af.acousticness) as avg_acousticness
 FROM dim_tracks t LEFT JOIN dim_audio_features af ON t.track_id = af.track_id
-WHERE t.release_year IS NOT NULL
-GROUP BY decade ORDER BY decade;
+WHERE t.release_date IS NOT NULL AND YEAR(t.release_date) BETWEEN 1900 AND 2025  -- Filtro de Anos
+GROUP BY decade
+ORDER BY decade;
 
 -- View: Músicas mais dançantes
 CREATE OR REPLACE VIEW vw_most_danceable_tracks AS
 SELECT 
     t.track_name,
     a.artist_name,
-    t.release_year,
+    t.release_date,                        
+    YEAR(t.release_date) as release_year,  
     af.danceability,
     af.energy,
     af.valence,
     t.popularity
 FROM dim_tracks t INNER JOIN dim_artists a ON t.artist_id = a.artist_id
 INNER JOIN dim_audio_features af ON t.track_id = af.track_id
-WHERE af.danceability > 0.7 ORDER BY af.danceability DESC, t.popularity DESC;
+WHERE af.danceability > 0.7
+ORDER BY af.danceability DESC, t.popularity DESC;
 
 SELECT * FROM vw_audio_features_stats;
-SELECT * FROM vw_tracks_complete;
+SELECT * FROM vw_tracks_complete LIMIT 10;
+SELECT * FROM vw_most_danceable_tracks LIMIT 10;
+SELECT * FROM vw_music_by_decade LIMIT 10;
+SELECT * FROM vw_top_artists_with_tracks LIMIT 10;
+SELECT * FROM vw_top_popular_tracks LIMIT 10;
 DESCRIBE dim_tracks;
